41c3006e90184167d375d4bac04bf94fbfc5aaca
Maryna M <markova.maryna@gmail.com>
2023-01-11 22:49:28 +0200

    add pieces till 72

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import RandomNuber from "./RandomNumber";
 // import FullBardo2_3 from "./BARDO2_3";
 // import Meditations from "./Meditations";
-import Piece70 from "./Piece70";
+import Piece72 from "./Piece72";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="Container">
-        <Piece70 />
+        <Piece72 />
         {/* <FullBardo2_3 /> */}
         {/* <Meditations /> */}
         {/* <RandomNuber /> */}
